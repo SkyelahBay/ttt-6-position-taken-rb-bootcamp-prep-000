52d2199727318eb14c72ed82f808a5ecbb6e529a
Skyelah Hanna Bay <skyelahbay@gmail.com>
2018-05-06 16:57:55 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 
 def position_taken?(board, pos)
   index = pos-1
-  if board[index] == " " || board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
